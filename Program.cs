940a5f21ef62a05a230c740d378691dadd1edc77
pawkal <kalel7410@gmail.com>
2023-01-25 19:15:37 +0100

    test

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace RandomNumbersConsole
             do
             {
 
-                Console.Write("Zgadnij Liczbę w przedziale 0 do 100 s :  ");
+                Console.Write("Zgadnij Liczbę w przedziale 0 do 100 :  ");
                 string p = Console.ReadLine();
 
                 int i = int.Parse(p);
