be0c32605f2f91bcdc021b66ba29e9593f1abd51
pawkal <kalel7410@gmail.com>
2023-01-25 19:11:35 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumbersConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+
+            do
+            {
+
+                Console.Write("Zgadnij numer w przedziale 0 do 100 :  ");
+                string p = Console.ReadLine();
+
+                int i = int.Parse(p);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("Za duża liczba zgaduj dalej!!");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("Liczba za niska zgaduj dalej!!");
+                }
+
+                else if (i == winNum)
+                {
+                    Console.WriteLine("Wygrałeś!!!");
+                    win = true;
+                }
+                Console.WriteLine();
+                
+               
+            } while (win == false);
+
+            Console.WriteLine("Dziękuje za gre!!!!!");
+
+            Console.WriteLine("Naciśnij dowolny klawisz żeby zakończyc");
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
